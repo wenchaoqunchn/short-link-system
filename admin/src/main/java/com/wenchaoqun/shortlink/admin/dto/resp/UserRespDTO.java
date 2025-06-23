688882051e2d1861aafdf61ad840a3441233bfee
Chaoqun Wen <wenchaoqunchn@gmail.com>
2025-06-23 17:42:01 +0800

    feat(UserDTO): 对手机号进行脱敏

--- admin/src/main/java/com/wenchaoqun/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/wenchaoqun/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.wenchaoqun.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wenchaoqun.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -26,6 +28,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
