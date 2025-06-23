dc826e4ec0a3315faf47ed5e408a7caba5bf154f
Chaoqun Wen <wenchaoqunchn@gmail.com>
2025-06-23 17:35:59 +0800

    feat(admin): 新增根据用户名获取未脱敏用户信息接口

--- admin/src/main/java/com/wenchaoqun/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/wenchaoqun/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.wenchaoqun.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.wenchaoqun.shortlink.admin.common.convention.result.Result;
 import com.wenchaoqun.shortlink.admin.common.convention.result.Results;
+import com.wenchaoqun.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.wenchaoqun.shortlink.admin.dto.resp.UserRespDTO;
 import com.wenchaoqun.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,11 @@ public class UserController {
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable String username){
         return Results.success(userService.getUserByUsername(username));
-
     }
+
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUsername(@PathVariable String username){
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username),UserActualRespDTO.class));
+    }
+
 }
