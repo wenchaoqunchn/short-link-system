007f654e37b26464bac5a3dd3d62f50c0705e4e4
Chaoqun Wen <wenchaoqunchn@gmail.com>
2025-06-25 19:48:34 +0800

    feat(enums): 新增四个用户相关异常码

--- admin/src/main/java/com/wenchaoqun/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/wenchaoqun/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -4,8 +4,13 @@ import com.wenchaoqun.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_NOT_EXIST("B000200", "用户不存在"),
-    USER_ALREADY_EXIST("B000201", "用户已存在");
+    USER_NOT_EXIST("B000200", "用户记录不存在"),
+    USER_NAME_REPEAT("B000201", "用户名已存在"),
+    USER_EXIST("B000202", "用户记录存在"),
+    USER_SAVE_ERROR("B000203", "用户记录新增失败"),
+    USER_UPDATE_ERROR("B000204", "用户记录更新失败"),
+    USER_LOGIN_ERROR("B000205","用户登录失败");
+
 
     private final String code;
 
