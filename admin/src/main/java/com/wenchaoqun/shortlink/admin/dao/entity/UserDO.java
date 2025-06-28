4c8d80b4aed147d803c0b8bca742b67c3ede7911
Chaoqun Wen <wenchaoqunchn@gmail.com>
2025-06-28 11:48:06 +0800

    refactor(DO): 优化持久层对象基础属性

--- admin/src/main/java/com/wenchaoqun/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/wenchaoqun/shortlink/admin/dao/entity/UserDO.java
@@ -1,11 +1,8 @@
 package com.wenchaoqun.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.wenchaoqun.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 /**
  * 用户持久层实体
@@ -13,7 +10,7 @@ import java.time.LocalDateTime;
 
 @Data
 @TableName("t_user")
-public class UserDO {
+public class UserDO extends BaseDO {
 
     /**
      * ID
@@ -49,23 +46,5 @@ public class UserDO {
      * 注销时间戳
      */
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private LocalDateTime updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 
 }
